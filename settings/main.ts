98dd3b178086aa03c978b97ea41291b6e407cb5f
pdhoward <patrick.howard@hotmail.com>
2026-01-13 12:42:16 -0600

    style updates

--- settings/main.ts
+++ settings/main.ts
@@ -1,25 +1,24 @@
-export const url = "https://rubix-documents.vercel.app"
-export const siteicon = "/icon.png"
+export const url = "https://voice-machine-docs.vercel.app"
+export const siteicon = "https://res.cloudinary.com/stratmachine/image/upload/v1592332363/machine/icon-512x512_zaffp5.png"
 
 export const gtm = "GTM-XXXXXXX"
 export const gtmconnected = true
 
-export const sitename = "Documents"
+export const sitename = "documentation"
 export const description =
-  "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements."
-export const keywords = [
-  "Next.js",
+  "Documentation for designing, building and deploying AI Voice Agents on the Strategic Machines Voice Platform"
+export const keywords = [  
   "documentation",
-  "Rubix Studios",
-  "open source",
-  "SEO",
+  "Strategic Machines",
+  "voice machine",
+  "AI Agents",
 ]
-export const urlimage = "/images/og-image.png"
-export const imagealt = "Rubix Studios Documentation"
-export const twitterhandle = "@RubixStudios"
+export const urlimage = "https://res.cloudinary.com/stratmachine/image/upload/v1592332363/machine/icon-512x512_zaffp5.png"
+export const imagealt = "Strategic Machines Documentation"
+export const twitterhandle = "@urban_coder"
 
-export const companyname = "Rubix Studios"
-export const companylink = "https://rubixstudios.com.au"
+export const companyname = "Strategic Machines"
+export const companylink = "https://strategicmachines.ai"
 
 export const branding = true
 export const rightsidebar = true
